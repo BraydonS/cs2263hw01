5166dab8aac3235779d8c54a7e856950101a97fa
BraydonS <braydonspaulding1@gmail.com>
2021-01-29 13:46:12 -0700

    Initial Setup. Add JavaFX

---
+++ app/src/main/java/cs2263hw01/Course.java
@@ -0,0 +1,14 @@
+package cs2263hw01;
+
+public class Course{
+
+    int courseNumber;
+    String courseName;
+    int courseCredits;
+
+    public Course(int coursenum, String name, int credits){
+        this.courseNumber = coursenum;
+        this.courseName = name;
+        this.courseCredits = credits;
+    }
+}
